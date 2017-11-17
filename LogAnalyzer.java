d29918f4b7369d5dc98f8d529c921d61b92f8d8a
Emeka Okonkwo <g00255702@stu.raritanval.edu>
2017-11-17 10:24:07 -0500

    7.18 & 7.19

--- LogAnalyzer.java
+++ LogAnalyzer.java
@@ -11,6 +11,12 @@ public class LogAnalyzer
 {
     // Where to calculate the hourly access counts.
     private int[] hourCounts;
+    // Where to calculate the day access counts
+    private int[] dayCounts;
+    // Where to calculate the month access counts
+    private int[] monthCounts;
+    // Where to calculate the year access counts
+    private int[] yearCounts;
     // Use a LogfileReader to access the data.
     private LogfileReader reader;
 
@@ -22,6 +28,10 @@ public class LogAnalyzer
         // Create the array object to hold the hourly
         // access counts.
         hourCounts = new int[24];
+        dayCounts = new int[28];
+        monthCounts = new int[12];
+        yearCounts = new int[4];
+        
         // Create the reader to obtain the data.
         reader = new LogfileReader(filename);
     }
@@ -49,7 +59,7 @@ public class LogAnalyzer
         int busiest = 0;
         for(int hour = 0; hour < hourCounts.length; hour++)
         {
-            if(hourCounts[busiest] < hourCounts[hour])
+            if(busiest < hourCounts[hour])
             {
                 busiest = hour;
             }
@@ -65,12 +75,130 @@ public class LogAnalyzer
         int quietest = 0;
         for(int hour = 0; hour < hourCounts.length; hour++)
         {
-            if(hourCounts[hour] < hourCounts[quietest])
+            if(hourCounts[hour] < quietest)
             {
                 quietest = hour;
             }
         }
         return quietest;
+    }
+    
+    /**
+     * Return the busiest two-hour period
+     */
+    public int busiestTwoHour()
+    {
+        int total = 0;
+        int totalHour = 0;
+        for(int hour = 0; hour < hourCounts.length -1; hour++)
+        {
+            if(total < (hourCounts[hour] + hourCounts[hour+1]))
+            {
+                total = hour;
+                totalHour = hourCounts[hour] + hourCounts[hour+1];
+            }
+        }
+        return totalHour;
+    }
+    
+    /**
+     * Return the quiestest Day
+     */
+    public int quietestDay()
+    {
+        int quietest = 0;
+        int quietestDay = 0;
+        for(int index = 0; index < dayCounts.length; index++)
+        {
+            if(dayCounts[index] < quietest)
+            {
+                quietest = dayCounts[index];
+                quietestDay = index;
+            }
+        }
+        return quietestDay;
+    }
+    
+    /**
+     * Return the busiest Day
+     */
+    public int busiestDay()
+    {
+        int busiest = 0;
+        int busiestDay = 0;
+        for(int index = 0; index < dayCounts.length; index++)
+        {
+            if(busiestDay < dayCounts[index])
+            {
+                busiest = dayCounts[index];
+                busiestDay = index;
+            }
+        }
+        return busiestDay;
+    }
+    
+    /**
+     * Return the total accesses per month
+     */
+    public int totalAccessesPerMonth()
+    {
+        int total = 0;
+        for(int index = 0; index < monthCounts.length; index++)
+        {
+            total = total + monthCounts[index];
+        }
+        return total;
+    }
+    
+    /**
+     * Return the quietest Month
+     */
+    public int quietestMonth()
+    {
+        int quietest = 0;
+        int quietestMonth = 0;
+        for(int index = 0; index < monthCounts.length; index++)
+        {
+            if(monthCounts[index] < quietest)
+            {
+                quietest = monthCounts[index];
+                quietestMonth = index;
+            }
+        }
+        return quietestMonth;
+    }
+    
+    /**
+     * Return the busiest Month
+     */
+    public int busiestMonth()
+    {
+        int busiest = 0;
+        int busiestMonth = 0;
+        for(int index = 0; index < monthCounts.length; index++)
+        {
+            if(busiest < monthCounts[index])
+            {
+                busiest = monthCounts[index];
+                busiestMonth = index;
+            }
+        }
+        return busiestMonth;
+    }
+    
+    /**
+     * Return the average Accesses Per Month
+     */
+    public int averageAccessesPerMonth()
+    {
+        int total = 0;
+        int average = 0;
+        for(int index = 0; index < monthCounts.length; index++)
+        {
+            total = monthCounts[index];
+            average = total/12;
+        }
+        return average;
     }
 
     /**
@@ -105,4 +233,5 @@ public class LogAnalyzer
     {
         reader.printData();
     }
+    
 }
--- LogfileCreator.java
+++ LogfileCreator.java
@@ -57,7 +57,7 @@ public class LogfileCreator
      */
     public LogEntry createEntry()
     {
-        int year = 2016;
+        int year = 2011 + rand.nextInt(4);
         int month = 1 + rand.nextInt(12);
         // Avoid the complexities of days-per-month.
         int day = 1 + rand.nextInt(28);
